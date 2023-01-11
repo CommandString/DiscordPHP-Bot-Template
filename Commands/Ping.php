10e2e279d61f07ee2a655b3182313dbf0d56105e
commandstring <bmxmikey2@gmail.com>
2023-01-10 23:52:51 -0500

    Updated Ping.php

--- Commands/Ping.php
+++ Commands/Ping.php
@@ -8,20 +8,17 @@ use Discord\Builders\MessageBuilder;
 use Discord\Parts\Interactions\Interaction;
 
 class Ping extends Template {
+    protected static string|array $name = "ping";
+
     public static function handler(Interaction $interaction): void
     {
         $interaction->respondWithMessage(MessageBuilder::new()->setContent('pong :ping_pong:'), true);
     }
 
-    public static function getName(): string
-    {
-        return "ping";
-    }
-
     public static function getConfig(): CommandBuilder|array
     {
         return (new CommandBuilder)
-            ->setName(self::getName())
+            ->setName(self::$name)
             ->setDescription("Ping the bot")
         ;
     }
