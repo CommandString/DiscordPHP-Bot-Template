f7becb9609611d346f28406ed4a1e1ff5c9cc443
commandstring <bmxmikey2@gmail.com>
2023-01-11 08:35:26 -0500

    Added action row for interaction examples

--- Commands/Ping.php
+++ Commands/Ping.php
@@ -2,8 +2,11 @@
 
 namespace Commands;
 
+use Classes\Utils;
 use Commands\Template;
 use Discord\Builders\CommandBuilder;
+use Discord\Builders\Components\ActionRow;
+use Discord\Builders\Components\Button;
 use Discord\Builders\MessageBuilder;
 use Discord\Parts\Interactions\Interaction;
 
@@ -12,7 +15,12 @@ class Ping extends Template {
 
     public static function handler(Interaction $interaction): void
     {
-        $interaction->respondWithMessage(MessageBuilder::new()->setContent('pong :ping_pong:'), true);
+        $interaction->respondWithMessage(MessageBuilder::new()->setContent('pong :ping_pong:')->addComponent(self::getActionRow()), true);
+    }
+
+    public static function getActionRow(): ActionRow
+    {
+        return Utils::buildActionRowWithButtons(Utils::newButton(Button::STYLE_PRIMARY, "Ping", "Ping"), Utils::newButton(Button::STYLE_SECONDARY, "Pong", "Pong"));
     }
 
     public static function getConfig(): CommandBuilder|array
