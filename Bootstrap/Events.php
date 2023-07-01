45df7c858846023b7419c7e4f2fad03e26ce4c42
Robert Snedeker <bmxmikey2@gmail.com>
2023-07-01 00:41:01 -0400

    Created event autoloader

---
+++ Bootstrap/Events.php
@@ -0,0 +1,39 @@
+<?php
+
+use Core\Events\Event;
+
+use function Core\discord;
+use function Core\doesClassHaveAttribute;
+use function Core\loopClasses;
+
+$events = [];
+
+loopClasses(BOT_ROOT . '/Core/Events', static function (string $className) use (&$events) {
+    if (!interface_exists($className) || $className === Event::class) {
+        return;
+    }
+
+    $attribute = doesClassHaveAttribute($className, Event::class);
+
+    if (!$attribute) {
+        return;
+    }
+
+    $events[$className] = $attribute->newInstance()->name;
+});
+
+loopClasses(BOT_ROOT . '/Events', static function (string $className) use ($events) {
+    $discord = discord();
+    $event = new $className();
+    $reflection = new ReflectionClass($event);
+
+    foreach ($reflection->getInterfaceNames() as $interface) {
+        $eventName = $events['\\' . $interface] ?? null;
+
+        if ($eventName === null) {
+            continue;
+        }
+
+        $discord->on($eventName, $event->handle(...));
+    }
+});
