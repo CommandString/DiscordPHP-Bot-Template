1a6e02fa23b6a8ac5aa6283c841404e256199091
Robert Snedeker <bmxmikey2@gmail.com>
2023-07-02 20:13:29 -0400

    Finished the command HMR system

--- Core/HMR/HotFile.php
+++ Core/HMR/HotFile.php
@@ -5,26 +5,43 @@ namespace Core\HMR;
 use Evenement\EventEmitter;
 use React\EventLoop\Loop;
 
+use function Core\discord;
+
 class HotFile extends EventEmitter
 {
     public const EVENT_HAS_CHANGED = 'hasChanged';
 
-    protected int $lastModified;
+    protected string $contents = '';
 
     public function __construct(
         public readonly string $file
     ) {
-        $this->lastModified = filemtime($file);
+        if (!file_exists($file)) {
+            discord()->getLogger()->error("File {$file} does not exist");
 
-        Loop::addTimer(1, function () {
+            return;
+        }
+
+        $this->contents = $this->getContents();
+
+        Loop::addPeriodicTimer(1, function () {
+            discord()->getLogger()->debug("Checking if file {$this->file} has changed");
+
             if ($this->hasChanged()) {
+                discord()->getLogger()->debug("File {$this->file} has changed");
+                $this->contents = $this->getContents();
                 $this->emit(self::EVENT_HAS_CHANGED, [$this]);
             }
         });
     }
 
+    public function getContents(): string
+    {
+        return file_get_contents($this->file);
+    }
+
     public function hasChanged(): bool
     {
-        return $this->lastModified !== filemtime($this->file);
+        return $this->contents !== $this->getContents();
     }
 }
