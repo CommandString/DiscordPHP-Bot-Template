7060006a90c263367f002fa634b53a06d021d0d9
Robert Snedeker <bmxmikey2@gmail.com>
2023-07-04 20:58:16 -0400

    Idek why this was here

--- Bot.php
+++ Bot.php
@@ -1,12 +1,7 @@
 <?php
 
-use Commands\Ping;
-
 use function Core\discord;
 
-const BOT_ROOT = __DIR__;
 require_once __DIR__ . '/Bootstrap/Requires.php';
-
-$reflection = new ReflectionClass(Ping::class);
 
 discord()->run(); // Run the bot
