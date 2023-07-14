f2561068c40a22ceab1c103cf970c26e63025fc4
Robert Snedeker <bmxmikey2@gmail.com>
2023-07-14 19:48:34 -0400

    Required Config

--- Bootstrap/Requires.php
+++ Bootstrap/Requires.php
@@ -1,5 +1,6 @@
 <?php
 
 require_once BOT_ROOT . '/vendor/autoload.php';
+require_once BOT_ROOT . '/Bootstrap/Config.php';
 require_once BOT_ROOT . '/Bootstrap/Environment.php';
 require_once BOT_ROOT . '/Bootstrap/Discord.php';
