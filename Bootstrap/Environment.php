231d1868ae9ab081ce1d8524e329d39d3131ed78
Robert Snedeker <rsnedeker20@gmail.com>
2023-07-16 14:50:29 -0400

    No longer needed

--- Bootstrap/Environment.php
+++ Bootstrap/Environment.php
@@ -9,9 +9,3 @@ $env = Env::createAuto();
 if (!isset($env->TOKEN)) {
     throw new RuntimeException('No token supplied to environment!');
 }
-
-if (!is_dir(BOT_ROOT . '/Core/HMR/Cached')) {
-    mkdir(BOT_ROOT . '/Core/HMR/Cached');
-}
-
-deleteAllFilesInDirectory(BOT_ROOT . '/Core/HMR/Cached');
