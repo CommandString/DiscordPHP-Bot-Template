0b9209603bfc34573a4f9dcf656fa2652a2d6c79
Robert Snedeker <bmxmikey2@gmail.com>
2023-07-16 06:52:32 -0400

    Removed HMR

--- Bootstrap/Commands.php
+++ Bootstrap/Commands.php
@@ -4,14 +4,14 @@ use Core\Commands\Command;
 use Core\Commands\CommandQueue;
 use Core\Commands\QueuedCommand;
 use Core\Disabled;
-use Core\HMR\HotCommand;
 
 use function Core\debug;
 use function Core\discord;
 use function Core\doesClassHaveAttribute;
+use function Core\error;
 use function Core\loopClasses;
 
-$commandQueue = Config::AUTO_REGISTER_COMMANDS ? new CommandQueue() : null;
+$commandQueue = new CommandQueue();
 $discord = discord();
 loopClasses(BOT_ROOT . '/Commands', static function (string $className) use ($commandQueue) {
     debug('Loading Command: ' . $className);
@@ -23,20 +23,10 @@ loopClasses(BOT_ROOT . '/Commands', static function (string $className) use ($co
         return;
     }
 
-    $commandQueue?->appendCommand(new QueuedCommand(
+    $commandQueue->appendCommand(new QueuedCommand(
         $attribute->newInstance(),
         new $className()
     ));
 });
 
-$commandQueue?->runQueue()->then(static function (array $commands) {
-    foreach ($commands as $name => &$command) {
-        $file = BOT_ROOT . '\\' . $command[0]->handler::class . '.php';
-
-        new HotCommand(
-            $name,
-            $command[1],
-            $file
-        );
-    }
-})->otherwise(static fn (Throwable $e) => $discord->getLogger()->error($e->getMessage()));
+$commandQueue->runQueue(registerCommands: Config::AUTO_REGISTER_COMMANDS)->otherwise(static fn (Throwable $e) => error($e->getMessage()));
